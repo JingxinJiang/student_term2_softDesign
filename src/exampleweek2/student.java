324edcfc2c842dd905a276c9ca221a10ebf96797
JingxinJiang <jiajingx@sheridancollege.ca>
2022-01-26 08:56:22 -0500

    Edit student class

--- src/exampleweek2/student.java
+++ src/exampleweek2/student.java
@@ -12,6 +12,29 @@ package exampleweek2;
 public class student {
     private String name;
     private int num;
+public student(){
+    name="lucy";
+    num=40;
+}
+public student(String name, int num){
+    this.name=name;
+    this.num=num;
+}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
     
     
 }
