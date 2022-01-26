39a690551bc58bb4a574abfcad4b60993dbfc544
JingxinJiang <jiajingx@sheridancollege.ca>
2022-01-26 10:08:19 -0500

    Add a part-time-student class

---
+++ src/exampleweek2/ParTimeStudent.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exampleweek2;
+
+/**
+ *
+ * @author seanz
+ */
+public class ParTimeStudent extends student{
+    private int numCourse;
+    public ParTimeStudent(String name, int num, int numCourse){
+        super(name, num); //call the constructor of student class
+        this.numCourse=numCourse;
+    }
+    public int getMunCourse() {
+        return munCourse;
+    }
+
+    public void setMunCourse(int munCourse) {
+        this.munCourse = munCourse;
+    }
+    
+    
+}
